af8bc66184c2fa7f48711e0034c1ae063f2d3ac8
FallenDios <francocabrera585@gmail.com>
2025-06-19 23:49:39 -0300

    Create Persona.php
    Creacion class Persona

---
+++ Persona.php
@@ -0,0 +1,35 @@
+<?php
+
+class Persona {
+    private $nombre;
+    private $apellido;
+    private $mensajeOperacion;
+
+    public function __construct($nombre = "", $apellido = "") {
+        $this->nombre = $nombre;
+        $this->apellido = $apellido;
+    }
+
+
+    public function cargar($nombre, $apellido) {
+        $this->setNombre($nombre);
+        $this->setApellido($apellido);
+    }
+
+    public function __toString() {
+        return "Nombre: " . $this->getNombre() . "\n" .
+               "Apellido: " . $this->getApellido() . "\n";
+    }
+
+
+    public function getNombre() { return $this->nombre; }
+    public function getApellido() { return $this->apellido; }
+    public function getMensajeOperacion() { return $this->mensajeOperacion; }
+
+    public function setNombre($nombre) { $this->nombre = $nombre; }
+    public function setApellido($apellido) { $this->apellido = $apellido; }
+    public function setMensajeOperacion($mensajeOperacion) {$this->mensajeOperacion = $mensajeOperacion;}
+}
+
+
+?>
